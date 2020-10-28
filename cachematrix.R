4f07825c8dc91d7b813bb97fbd7eff54058e3eb2
Juveriya Shaikh <juveriyash05@gmail.com>
2020-10-28 18:56:25 +0530

    Solution placed in cacheMatrix.R file contained in the git repository. Few errors fixed

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
   }
   
   get <- function()x
-  setInverse <- function(inverse)j <<- inverse
+  setInverse <- function(inverse)m <<- inverse
   getInverse <- function()m
   list(set = set , get = get , setInverse = setInverse , getInverse = getInverse)
 }
